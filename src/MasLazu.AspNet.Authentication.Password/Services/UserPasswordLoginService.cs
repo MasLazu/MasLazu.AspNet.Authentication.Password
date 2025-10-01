ee084a5f9fcf91641dc3983114bd66d6f102ded2
mfaziz <mfaziz50@gmail.com>
2025-10-01 13:44:47 +0700

    fix: Update MasLazu Framework package versions and modify user login service method signatures

--- src/MasLazu.AspNet.Authentication.Password/Services/UserPasswordLoginService.cs
+++ src/MasLazu.AspNet.Authentication.Password/Services/UserPasswordLoginService.cs
@@ -90,14 +90,14 @@ public class UserPasswordLoginService : CrudService<UserPasswordLogin, UserPassw
             GenderCode: null
         );
 
-        UserDto userDto = await _userService.CreateAsync(Guid.Empty, createUserRequest, ct);
+        UserDto userDto = await _userService.CreateAsync(Guid.Empty, createUserRequest, false, ct);
 
         var createLoginMethodRequest = new CreateUserLoginMethodRequest(
             UserId: userDto.Id,
             LoginMethodCode: PasswordConstants.LoginMethodCode
         );
 
-        UserLoginMethodDto userLoginMethodDto = await _userLoginMethodService.CreateAsync(Guid.Empty, createLoginMethodRequest, ct);
+        UserLoginMethodDto userLoginMethodDto = await _userLoginMethodService.CreateAsync(Guid.Empty, createLoginMethodRequest, false, ct);
 
         var userPasswordLogin = new UserPasswordLogin
         {
